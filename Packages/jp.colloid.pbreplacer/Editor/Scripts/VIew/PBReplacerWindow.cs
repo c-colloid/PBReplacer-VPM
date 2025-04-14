bc584e7d23366c8127857aa8365161f441c5bdf5
colloid <jack0904h3@yahoo.co.jp>
2025-04-15 04:36:44 +0900

    Revert "Prefabの検索テスト"
    This reverts commit 0d5b96c3f316a4fdb8da002786c1112b693ea0e0.

--- Packages/jp.colloid.pbreplacer/Editor/Scripts/VIew/PBReplacerWindow.cs
+++ Packages/jp.colloid.pbreplacer/Editor/Scripts/VIew/PBReplacerWindow.cs
@@ -471,9 +471,6 @@ namespace colloid.PBReplacer
 		private void OnAvatarFieldValueChanged(ChangeEvent<UnityEngine.Object> evt)
 		{
 			var avatarObject = evt.newValue as Component;
-			
-			Debug.Log(PrefabUtility.IsPartOfAnyPrefab(avatarObject));
-			Debug.Log(PrefabUtility.GetNearestPrefabInstanceRoot(avatarObject));
             
 			AvatarFieldHelper.SetAvatar(avatarObject?.gameObject);
                 
